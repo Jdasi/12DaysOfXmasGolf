c95a993ced29c77bebb05f7cd36030e64249de4e
Joe da Silva <silvacore@hotmail.co.uk>
2017-12-20 02:44:55 +0000

    600

--- 12DaysOfChristmas/main.cpp
+++ 12DaysOfChristmas/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     char*n[]{"First","Second","Third","Fourth","Fifth","Sixth","Seventh","Eighth","Ninth","Tenth","Eleventh","Twelveth"};
-    char*g[]{"a Partridge in a Pear Tree.\n","Two Turtle Doves\n","Three French Hens,\n","Four Calling Birds,\n","Five Gold Rings,\n","Six Geese a-Lay","Seven Swans a-Swimm","Eight Maids a-Milk","Nine Ladies Danc","Ten Lords a-Leap","Eleven Pipers Pip","Twelve Drummers Drumm"};
+    char*g[]{"a Partridge in a Pear Tree.","Two Turtle Doves","Three French Hens,","Four Calling Birds,","Five Gold Rings,","Six Geese a-Lay","Seven Swans a-Swimm","Eight Maids a-Milk","Nine Ladies Danc","Ten Lords a-Leap","Eleven Pipers Pip","Twelve Drummers Drumm"};
 
     for (int i=0;i<12;++i)
     {
@@ -16,7 +16,9 @@ int main()
 
             c g[j];
 
-            if(j>4)c"ing,\n";
+            if(j>4)c"ing,";
+
+            c"\n";
         }
 
         c"\n";
