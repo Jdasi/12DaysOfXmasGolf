4ccb2a7810b3a0ce84c8c224682965bfbe4fac49
Joe da Silva <silvacore@hotmail.co.uk>
2017-12-20 02:21:01 +0000

    606

--- 12DaysOfChristmas/main.cpp
+++ 12DaysOfChristmas/main.cpp
@@ -4,20 +4,19 @@
 int main()
 {
     char*n[]{"First","Second","Third","Fourth","Fifth","Sixth","Seventh","Eighth","Ninth","Tenth","Eleventh","Twelveth"};
-    char*s[]{"On the "," day of Christmas my true love sent to me\n"};
     char*g[]{"a Partridge in a Pear Tree.\n","Two Turtle Doves\n","Three French Hens,\n","Four Calling Birds,\n","Five Gold Rings,\n","Six Geese a-Lay","Seven Swans a-Swimm","Eight Maids a-Milk","Nine Ladies Danc","Ten Lords a-Leap","Eleven Pipers Pip","Twelve Drummers Drumm"};
 
     for (int i=0;i<12;++i)
     {
-        c s[0] << n[i] << s[1];
+        c"On the "<<n[i]<<" day of Christmas my true love sent to me\n";
 
         for (int j=i;j>=0;--j)
         {
-            if (j==0&&i>0)c"and ";
+            if(j==0&&i>0)c"and ";
 
             c g[j];
 
-            if (j > 4) c"ing,\n";
+            if(j>4)c"ing,\n";
         }
 
         c"\n";
